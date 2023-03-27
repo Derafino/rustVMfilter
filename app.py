6e5073bfa774cbf62d17c6330a70ec79fe27d190
Derafino <danya55555game@gmail.com>
2023-03-28 00:40:29 +0300

    RustItem model and img

--- app.py
+++ app.py
@@ -4,45 +4,46 @@ import json
 from flask import Flask, abort, request, render_template
 from sqlalchemy import desc
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import aliased
 
 from database import db
+from get_items import retrieve_rust_items
 from marker_parser import get_info
-from models import Shop, SellOrder
+from models import Shop, SellOrder, RustItem
+from flask_migrate import Migrate
 
 
-def complete_shops_info(shops):
-    with open("items.json", "r") as input_file:
-        items = json.load(input_file)
-    shops_with_item_names = list()
-    for shop in shops:
-        orders = list()
-        for sell_order in shop.sell_orders:
-            sell_item = ''
-            currency = ''
-            for item in items:
-                if item['item_id'] == sell_order.item_id:
-                    sell_item = item['name']
-                if item['item_id'] == sell_order.currency_id:
-                    currency = item['name']
-                if sell_item and currency:
-                    orders.append({
-                        'item_name': sell_item,
-                        'currency_name': currency,
-                        'quantity': sell_order.quantity,
-                        'cost_per_item': sell_order.cost_per_item,
-                        'item_is_blueprint': sell_order.item_is_blueprint,
-                        'currency_is_blueprint': sell_order.currency_is_blueprint,
-                        'shop_id': shop.id})
-                    break
-        shops_with_item_names.append({'id': shop.id, 'name': shop.name, 'sell_orders': orders})
+def update_items_db():
+    items_json_file = retrieve_rust_items()
+    with open(items_json_file) as f:
+        try:
+            items = json.load(f)
 
-    return shops_with_item_names
+        except json.JSONDecodeError as e:
+            print(f'Error loading {items_json_file}: {e}')
+
+    update_mappings = []
+    for record in items:
+        if RustItem.query.filter_by(itemid=record['itemid']).first():
+            update_mappings.append(record)
+    db.session.bulk_update_mappings(RustItem, update_mappings)
+
+    insert_mappings = []
+    for record in items:
+        if not RustItem.query.filter_by(itemid=record['itemid']).first():
+            insert_mappings.append(record)
+    try:
+        db.session.bulk_insert_mappings(RustItem, insert_mappings)
+        db.session.commit()
+    except SQLAlchemyError:
+        abort(500, message="An error occurred while inserting the shop.")
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
     db.init_app(app)
+    migrate = Migrate(app, db)
     with app.app_context():
         db.create_all()
 
@@ -52,23 +53,39 @@ def create_app():
         asyncio.set_event_loop(loop)
         rust_markers = loop.run_until_complete(get_info.get_markers())
         shops = [marker for marker in rust_markers if marker.type == 3]
-        shops = complete_shops_info([shop for shop in shops if shop.sell_orders])
+
+        db.session.query(Shop).delete()
         db.session.query(SellOrder).delete()
         db.session.commit()
         for s in shops:
-            shop = Shop(id=s['id'], name=s['name'])
+            shop = Shop(id=s.id, name=s.name)
             try:
                 db.session.merge(shop)
                 db.session.commit()
             except SQLAlchemyError:
                 abort(500, message="An error occurred while inserting the shop.")
 
-            new_orders = [SellOrder(**order) for order in s['sell_orders']]
+            new_orders = [SellOrder(
+                item_id=order.item_id,
+                quantity=order.quantity,
+                currency_id=order.currency_id,
+
+                cost_per_item=order.cost_per_item,
+                item_is_blueprint=order.item_is_blueprint,
+                currency_is_blueprint=order.currency_is_blueprint,
+                amount_in_stock=order.amount_in_stock,
+                shop_id=s.id
+            ) for order in s.sell_orders]
             try:
                 db.session.bulk_save_objects(new_orders)
                 db.session.commit()
             except SQLAlchemyError:
                 abort(500, message="An error occurred while inserting orders.")
+        return 'Updated'
+
+    @app.route('/update_items')
+    def update_items():
+        update_items_db()
         return 'Updated'
 
     @app.route('/')
@@ -82,12 +99,13 @@ def create_app():
         currency_name = request.args.get('currency_name')
         query = db.session.query(SellOrder)
         if item_name:
-            query = query.filter(SellOrder.item_name.contains(item_name))
+            item_alias = aliased(RustItem)
+            query = query.join(item_alias, SellOrder.item).filter(item_alias.name.contains(item_name))
         if currency_name:
-            query = query.filter(SellOrder.currency_name.contains(currency_name))
+            currency_alias = aliased(RustItem)
+            query = query.join(currency_alias, SellOrder.currency).filter(currency_alias.name.contains(currency_name))
         query = query.order_by(desc(SellOrder.quantity / SellOrder.cost_per_item))
         results = query.all()
-        results = [SellOrder.query.filter_by(id=r.id).first() for r in results]
         return render_template('search_result.html', sell_orders=results, count=len(results))
 
     return app
--- models.py
+++ models.py
@@ -3,13 +3,23 @@ from database import db
 
 class SellOrder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    item_name = db.Column(db.String(100), nullable=False)
-    currency_name = db.Column(db.String(100), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('rust_item.itemid'))
     quantity = db.Column(db.Integer)
+    currency_id = db.Column(db.Integer, db.ForeignKey('rust_item.itemid'))
     cost_per_item = db.Column(db.Integer)
     item_is_blueprint = db.Column(db.Boolean)
     currency_is_blueprint = db.Column(db.Boolean)
+    amount_in_stock = db.Column(db.Integer)
     shop_id = db.Column(db.Integer, db.ForeignKey('shop.id'))
+
+    item = db.relationship('RustItem', foreign_keys=[item_id])
+    currency = db.relationship('RustItem', foreign_keys=[currency_id])
+
+
+class RustItem(db.Model):
+    itemid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(200), nullable=False)
 
 
 class Shop(db.Model):
